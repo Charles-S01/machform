2d12e45338f3c7f71a436d29a005d4fe7217888d
Charles Sangalang <charlesdenzel01@gmail.com>
2025-05-15 16:08:12 -0400

    Update custom.js

--- public/custom.js
+++ public/custom.js
@@ -3,11 +3,14 @@ $(document).ready(() => {
     function validate(value) {
         return false
     }
-
+    
     $("#element_1").on("blur", () => {
         var value = $(this).val()
+        console.log('Lost focus on element_1 field')
+        console.log(value)
         if (!validate(value)) {
             alert(`Cannot enter ${value}`)
+            console.log(`Cannot enter ${value}`)
         }
     })
 })
