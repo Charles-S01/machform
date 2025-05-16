51466721e82d78a1528341c194605bc5c1be7f0b
Charles Sangalang <charlesdenzel01@gmail.com>
2025-05-16 09:11:45 -0400

    Update custom.js

--- public/custom.js
+++ public/custom.js
@@ -1,16 +1,23 @@
 $(document).ready(() => {
-    console.log("Ready!")
+    console.log("Ready!!!")
+    
     function validate(value) {
+
         return false
     }
     
-    $("#element_1").on("blur", () => {
-        var value = $(this).val()
-        console.log('Lost focus on element_1 field')
+    $("#element_1").change(() => {
+        const value = $("#element_1").val()
+        console.log('element_1 field changed')
         console.log(value)
         if (!validate(value)) {
             alert(`Cannot enter ${value}`)
             console.log(`Cannot enter ${value}`)
         }
     })
+
+    $("form").submit(() => {
+        const entry1 = $("#element_1").val()
+        alert(`Field 1 entry: ${entry1}`);
+    });
 })
